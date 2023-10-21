dafc186df0776db5d4e4149e75cc7450291b5513
levi-tc <petrisori356@gmail.com>
2023-10-21 15:49:44 +0300

    .

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ import openai
 openai.api_key = "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"
 
 #Mesajul care e trimis catre chat-gpt
-message = ""
+message = "give me a hi five"
 
 request = openai.ChatCompletion.create(
     model = "gpt-3.5-turbo",
