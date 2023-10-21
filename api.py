cb656ebb36d3c1a343a09e918f6ebc61cde3213e
levi-tc <petrisori356@gmail.com>
2023-10-21 17:54:33 +0300

    SETUP COMPLETE

--- api.py
+++ api.py
@@ -1,36 +1,14 @@
-#import openai
-
-#openai.api_key = "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"
-
-#Mesajul care e trimis catre chat-gpt
-#message = "give me a hi five"
-
-#request = openai.ChatCompletion.create(
- #   model = "gpt-3.5-turbo",
-  #  messages = [
-    #    {
-   #        "role": "user",
-    #        "content": message
-     #   }
-   # ]
-   # )
-
-#raspunsul lui chat gpt
-#response = request['choices'][0]['message']['content']
-
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 
 import os
 from dotenv import load_dotenv
+load_dotenv()
 
-load_dotenv()  # Load environment variables from .env file
+# Set the secret key
+secret_key = os.getenv("OPENAI_SECRET_KEY")
 
-api_key = os.getenv("OPENAI_API_KEY")  # Replace "API_KEY" with the name of your API key variable in .env file
-
-
-
-
-chat_model = ChatOpenAI()
-
+# Set the llm and chat models
+llm = OpenAI(openai_api_key=secret_key)
+chat_models = ChatOpenAI()
 
