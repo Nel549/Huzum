7f04a93f633142548a1df0ea91a89b373233d473
Student USO VM User <student@stud.acs.upb.ro>
2023-10-22 01:01:03 +0300

    update

--- webapplication/base/models.py
+++ webapplication/base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class Message(models.Model):
+    msg = models.TextField()
+
+
+    def __str__(self):
+        return self.msg
--- webapplication/base/views.py
+++ webapplication/base/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
+from .forms import main_form
+from .models import Message
 
 # Create your views here.
 
 def index(request):
-    context = {}
+    form = main_form()
+    #Message.objects.all().delete()
+    context = {
+        'form': form,
+        'msgs': Message.objects.all()
+    }
+    if request.method == 'POST':
+        form = main_form(request.POST)
+        msg = request.POST.get('input_msg')
+        print(msg)
+
+        if form.is_valid():
+            save_msg = Message.objects.create(
+                msg = "Mesaj",
+                
+            )    
+            
+            save_msg.save()
 
     return render(request, 'base/index.html', context)
+
