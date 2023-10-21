8bc391ea632242e09e3b24cb50a12390c3f85d2d
levi-tc <petrisori356@gmail.com>
2023-10-21 16:44:18 +0300

    Help

--- api.py
+++ api.py
@@ -1,21 +1,30 @@
-import openai
+#import openai
 
-openai.api_key = "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"
+#openai.api_key = "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"
 
 #Mesajul care e trimis catre chat-gpt
-message = "give me a hi five"
+#message = "give me a hi five"
 
-request = openai.ChatCompletion.create(
-    model = "gpt-3.5-turbo",
-    messages = [
-        {
-            "role": "user",
-            "content": message
-        }
-    ]
-    )
+#request = openai.ChatCompletion.create(
+ #   model = "gpt-3.5-turbo",
+  #  messages = [
+    #    {
+   #        "role": "user",
+    #        "content": message
+     #   }
+   # ]
+   # )
 
 #raspunsul lui chat gpt
-response = request['choices'][0]['message']['content']
+#response = request['choices'][0]['message']['content']
 
-print(response)
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+
+llm = OpenAI(openai_api_key="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")
+chat_model = ChatOpenAI()
+
+
+text = "what is a good name for my pet who is a dog?"
+
+print(chat_model.predict(text))
