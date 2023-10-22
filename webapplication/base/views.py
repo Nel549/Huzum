e0a8f3bb81c57698fae1d4a6346da78e87b030da
levi-tc <petrisori356@gmail.com>
2023-10-22 04:19:16 +0300

    sj

--- webapplication/base/views.py
+++ webapplication/base/views.py
@@ -1,25 +1,31 @@
 from django.shortcuts import render, redirect
 from .forms import main_form
 from .models import Message
+from django.http import HttpResponse
 
 # Create your views here.
 
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+
 def index(request):
     form = main_form()
-    #Message.objects.all().delete()
     context = {
         'form': form,
         'msgs': Message.objects.all()
     }
     if request.method == 'POST':
         form = main_form(request.POST)
-        #msg = request.POST.get('input_msg')
+        msg = request.POST.get('input_msg')
 
         if form.is_valid():
-            main_form.save()
-            return redirect(' ')
-
-
+            form.save()  # create an instance of the form and call save() on it
+            data = {
+                'msg': msg,
+                'class': 'left-msg'
+            }
+            html = render_to_string('base/message.html', {'data': data})
+            return JsonResponse({'html': html})
 
     return render(request, 'base/index.html', context)
 
