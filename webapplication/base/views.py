029e528c1da8c3ea1f0ac4262d13b49c5ca9b74f
Student USO VM User <student@stud.acs.upb.ro>
2023-10-22 05:04:26 +0300

    front-end

--- webapplication/base/views.py
+++ webapplication/base/views.py
@@ -9,23 +9,30 @@ from django.http import JsonResponse
 from django.template.loader import render_to_string
 
 def index(request):
+    #Message.objects.all().delete()
+    last_msg = Message.objects.last()
+
     form = main_form()
+    msgs = Message.objects.all()
     context = {
         'form': form,
-        'msgs': Message.objects.all()
+        'msgs': msgs,
+        'last': msgs
     }
     if request.method == 'POST':
         form = main_form(request.POST)
         msg = request.POST.get('input_msg')
 
         if form.is_valid():
-            form.save()  # create an instance of the form and call save() on it
-            data = {
-                'msg': msg,
-                'class': 'left-msg'
-            }
-            html = render_to_string('base/message.html', {'data': data})
-            return JsonResponse({'html': html})
+            form.save()  
+            last_msg = Message.objects.last()# create an instance of the form and call save() on it
+            context = {
+            'form': form,
+            'msgs': msgs,
+            'last': msgs
+        }
+
+        print(msgs[::-1][0])
 
     return render(request, 'base/index.html', context)
 
